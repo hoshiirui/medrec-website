494668cf4f9487c793adb3dc4c80505f6105225d
hoshiirui <kotomaru7969@gmail.com>
2022-12-21 13:42:41 +0800

    test first

---
+++ templates/sidebar.php
@@ -0,0 +1,52 @@
+<div class="app-sidebar">
+    <div class="logo">
+        <a href="index.html" class="logo-icon"><span class="logo-text">Neptune</span></a>
+        <div class="sidebar-user-switcher user-activity-online">
+            <a href="#">
+                <img src="assets/images/avatars/avatar.png">
+                <span class="activity-indicator"></span>
+                <span class="user-info-text">Chloe<br><span class="user-state-info">On a call</span></span>
+            </a>
+        </div>
+    </div>
+    <div class="app-menu">
+        <ul class="accordion-menu">
+            <li class="sidebar-title">
+                Apps
+            </li>
+            <li class="active-page">
+                <a href="index.html" class="active"><i class="material-icons-two-tone">dashboard</i>Dashboard</a>
+            </li>
+            <li>
+                <a href="mailbox.html"><i class="material-icons-two-tone">inbox</i>Data Pasien</a>
+            </li>
+            <li>
+                <a href="file-manager.html"><i class="material-icons-two-tone">cloud_queue</i>Kunjungan Pasien</a>
+            </li>
+            <li>
+                <a href="calendar.html"><i class="material-icons-two-tone">calendar_today</i>Laporan Kunjungan</a>
+            </li>
+            <li>
+                <a href="todo.html"><i class="material-icons-two-tone">done</i>Help</a>
+            </li>
+            <li class="sidebar-title">
+                UI Elements
+            </li>
+            <li>
+                <a href="#"><i class="material-icons-two-tone">grid_on</i>Tables<i class="material-icons has-sub-menu">keyboard_arrow_right</i></a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="tables-basic.html">Basic</a>
+                    </li>
+                    <li>
+                        <a href="tables-datatable.html">DataTable</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="calendar.html"><i class="material-icons-two-tone">calendar_today</i>Calendar<span class="badge rounded-pill badge-success float-end">14</span></a>
+            </li>
+            
+        </ul>
+    </div>
+</div>
